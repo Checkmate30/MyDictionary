4b0188970df632103c57fbdbc20cbbe076676ac2
Checkmate30 <vveksawant@gmail.com>
2023-07-10 23:42:23 +0530

    history page created and added JS for history btn click

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// Function for history button clicked.
+function historyButton() {
+    window.location.href = 'history.html';
+}
+
+// Function for Search button clicked to come back to search page.
+function searchButton() {
+    window.location.href = 'index.html';
+}
